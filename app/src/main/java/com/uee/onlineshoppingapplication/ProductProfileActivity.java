7b8c7a3391b7ccc737b9012f13dc0a520f2cc4a8
aparna-jay <aparna.jayawardena1999@gmail.com>
2021-09-18 14:31:26 +0530

    Add product profile

---
+++ app/src/main/java/com/uee/onlineshoppingapplication/ProductProfileActivity.java
@@ -0,0 +1,14 @@
+package com.uee.onlineshoppingapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_profile);
+    }
+}
