47268c4246bd1ce5a2a528aaf8fb814039ca0123
aparna-jay <aparna.jayawardena1999@gmail.com>
2021-10-08 16:38:48 +0530

    Set minimum cart item quantity to one

--- app/src/main/java/com/uee/onlineshoppingapplication/ProductProfileActivity.java
+++ app/src/main/java/com/uee/onlineshoppingapplication/ProductProfileActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -25,7 +26,7 @@ public class ProductProfileActivity extends AppCompatActivity {
     TextView name, price, quantity, description;
     DatabaseReference databaseCarts;
     Button addToCart;
-    String userID = "temp";
+    String userID;
     String itemName, itemPrice, image, itemDescription;
     ImageView imageView;
     ImageButton plus, minus;
@@ -36,6 +37,13 @@ public class ProductProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_profile);
+
+        if (LoginActivity.loggedUser == null){
+            userID = "temp";
+        }
+        else {
+            userID = LoginActivity.loggedUser;
+        }
 
         Bundle extras = getIntent().getExtras();
 
@@ -71,14 +79,26 @@ public class ProductProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 totalQuantity = totalQuantity + 1;
-                quantity.setText(String.valueOf(totalQuantity));
+                if(totalQuantity <= 0) {
+                    quantity.setText("1");
+                    totalQuantity = 1;
+                }
+                else{
+                    quantity.setText(String.valueOf(totalQuantity));
+                }
             }
         });
         minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 totalQuantity = totalQuantity - 1;
-                quantity.setText(String.valueOf(totalQuantity));
+                if(totalQuantity <= 0) {
+                    quantity.setText("1");
+                    totalQuantity = 1;
+                }
+                else{
+                    quantity.setText(String.valueOf(totalQuantity));
+                }
             }
         });
     }
